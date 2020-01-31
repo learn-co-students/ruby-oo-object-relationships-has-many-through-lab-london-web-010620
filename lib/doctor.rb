1fdba62763fa6f53701727343ec02bcaf66e45d1
RikBala <rik280491@googlemail.com>
2020-01-31 09:24:37 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,34 @@
+require 'pry'
+class Doctor 
+    attr_accessor :name 
+
+    @@all = [] 
+
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def new_appointment(patient, date)
+        Appointment.new(patient, date, self)
+    end 
+
+    def appointments
+        Appointment.all.select{|appointment| appointment.doctor == self} 
+        # binding.pry
+    end 
+
+    def patients 
+        appointments.map{|appointment| appointment.patient}
+        # binding.pry
+    end 
+
+
+
+
+
+end 
---
+++ lib/patient.rb
@@ -0,0 +1,34 @@
+require 'pry'
+class Patient 
+
+    attr_accessor :name 
+
+    @@all = [] 
+
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end 
+
+    def self.all
+        @@all 
+    end 
+
+    def new_appointment(doctor, date)
+        Appointment.new(self, date, doctor)
+
+    end 
+
+    def appointments 
+        #binding.pry
+        Appointment.all.select{|appointment| appointment.patient == self }
+        
+    end 
+
+    def doctors 
+        appointments.map{|appointment| appointment.doctor }
+        #binding.pry 
+    end 
+
+
+end 
